b60b15afce549b9e252d30820fdb05a81de57295
Alexis Nowikowski <nowikowski22@hotmail.com>
2015-12-10 18:51:28 +0100

    HttpServer: basic start/stop methods implementation with server thread

--- HttpServer.h
+++ HttpServer.h
@@ -5,9 +5,26 @@
 #ifndef TIN_IPTABLES_HTTPSERVER_H
 #define TIN_IPTABLES_HTTPSERVER_H
 
+#include <string>
+#include <netinet/in.h>
 
-class HttpServer {
+using namespace std;
 
+class HttpServer
+{
+public:
+    HttpServer(unsigned short port);
+    HttpServer(string ipAddress, unsigned short port);
+    ~HttpServer();
+    void start();
+    void stop();
+    bool isRunning();
+    bool bIsRunning;
+private:
+    sockaddr_in localAddress;
+    pthread_t serverThread;
+    void* serverThreadWork();
+    static void *serverThreadWorkHelper(void *context);
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
+#include <unistd.h>
 
 #include "Configuration.h"
+#include "HttpServer.h"
 
 using namespace std;
 
 int main()
 {
-    Configuration::getInstance().Initialize("iptables.conf");
+    Configuration &configurationInstance = Configuration::getInstance();
+    configurationInstance.initialize("iptables.conf");
+
+    HttpServer server(configurationInstance.getServerIpAddress(), configurationInstance.getServerPort());
+
+    server.start();
+
     cout << "Hello, World!" << endl;
+
+    while(true) //NOT RECEIVED EXIT SIGNAL OR COMMAND
+    {
+        sleep(1);
+    }
+
+    server.stop();
+
     return 0;
 }
