883bc6d270f8c8410201586ef82ff9a8302fb79c
pawello2222 <pawello2222@gmail.com>
2015-12-14 19:30:28 +0100

    Added iptables commands

--- IPTablesExecutor.cpp
+++ IPTablesExecutor.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "IPTablesExecutor.h"
+
+IPTablesExecutor::IPTablesExecutor()
+{
+
+}
+
+std::string IPTablesExecutor::exec( const char* cmd )
+{
+    std::shared_ptr< FILE > pipe( popen( cmd, "r" ), pclose );
+
+    if ( !pipe )
+        return "ERROR";
+
+    char buffer[ 128 ];
+    std::string result = "";
+
+    while ( !feof( pipe.get() ) )
+    {
+        if ( fgets( buffer, 128, pipe.get() ) )
+            result += buffer;
+    }
+
+    return result;
+}
