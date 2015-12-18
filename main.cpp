b95772dfc74d5458a0cff11f302de7d300ee845e
root <marekm2504@gmail.com>
2015-12-18 16:21:52 +0100

    idziemy do cabaja

--- main.cpp
+++ main.cpp
@@ -90,6 +90,7 @@ int main(int argc, char *argv[])
 
 
     Configuration &configurationInstance = Configuration::getInstance();
+    std::cout<<configurationInstance.getHostName();
     if (commandLineArguments.IsConfigFilePathSet())
     {
         configurationInstance.initialize(commandLineArguments.GetConfigFilePath());
